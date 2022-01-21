a2f49a46331ba3855bbc2d0b7cbc5965435eb563
Marc <iammarcmason@googlemail.com>
2022-01-21 21:58:29 +0000

    sorted admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
+  eleventyConfig.addPassthroughCopy("./src/admin");
+
   return {
     dir: {
       input: "src",
