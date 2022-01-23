abb4fd8583cd407db7897531e3be46f45bc78f50
Marc <iammarcmason@googlemail.com>
2022-01-23 19:44:15 +0000

    modJS

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
-<script>
-
-  var codeElements = document.querySelectorAll('pre');
+var codeElements = document.querySelectorAll("pre");
 
 for (var i = 0; i < codeElements.length; i++) {
-   codeElements[i].classList.add('line-numbers');}
-
-
-
-</script>
+  codeElements[i].classList.add("line-numbers");
+}
