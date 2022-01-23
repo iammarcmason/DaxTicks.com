8c857c2924311a674967844eef7c6c72ad9e3fa1
Marc <iammarcmason@googlemail.com>
2022-01-23 19:41:55 +0000

    move js

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+<script>
+
+  var codeElements = document.querySelectorAll('pre');
+
+for (var i = 0; i < codeElements.length; i++) {
+   codeElements[i].classList.add('line-numbers');}
+
+
+
+</script>
